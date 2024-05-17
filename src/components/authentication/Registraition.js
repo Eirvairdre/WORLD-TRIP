a6c5b97df7999e30cebde854232e718f257aefd0
Eirvairdre <107425024+Eirvairdre@users.noreply.github.com>
2024-05-17 17:30:13 +1000

    routes

---
+++ src/components/authentication/Registraition.js
@@ -0,0 +1,12 @@
+import './registration.css';
+import React, {useState} from "react";
+
+function Registraiton() {
+    return(
+        <div className='Registration_Page'>
+            Registration
+        </div>
+    );
+}
+
+export default Registraiton;
